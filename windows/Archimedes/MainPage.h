c46946c33f9896d8f91fbb65d32c8f9a0dd5f633
EvanSymonds <evan.symonds@gmail.com>
2021-02-03 17:51:48 +0000

    Initialise project

---
+++ windows/Archimedes/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Archimedes::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Archimedes::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
